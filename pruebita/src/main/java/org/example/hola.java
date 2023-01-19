ef0f7b218d5102ace3761fa918019595c4d90e30
Javier Garzon <9005612@alumnos.ufv.es>
2023-01-19 22:32:46 +0100

    Creación clase hola

---
+++ pruebita/src/main/java/org/example/hola.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class hola {
+    String hh;
+    int hhs;
+
+    public hola(String hh, int hhs) {
+        this.hh = hh;
+        this.hhs = hhs;
+    }
+
+    public String getHh() {
+        return hh;
+    }
+
+    public void setHh(String hh) {
+        this.hh = hh;
+    }
+
+    public int getHhs() {
+        return hhs;
+    }
+
+    public void setHhs(int hhs) {
+        this.hhs = hhs;
+    }
+}
