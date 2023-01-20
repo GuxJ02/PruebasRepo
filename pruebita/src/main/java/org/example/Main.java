d64b39794cefcaf7ffc273b3c62f1e001dd4ec6b
Javier Garzon <9005612@alumnos.ufv.es>
2023-01-20 10:10:27 +0100

    pequeños cambios

--- pruebita/src/main/java/org/example/Main.java
+++ pruebita/src/main/java/org/example/Main.java
@@ -3,6 +3,9 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("algo aqui");
+
         System.out.println("djdjdj");
+        System.out.println("jdjdjd");
     }
 }
